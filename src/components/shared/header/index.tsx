fde9f82a348bfa17fe95890016715e1b551f7eca
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-04 16:11:45 +0530

    added theme toggler for theme

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,9 +1,11 @@
+"use client"
 import React from 'react'
 import { ShoppingCartIcon, UserIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
+import ModeToggle from './mode-toggle'
 
 
 const Header = () => {
@@ -19,12 +21,13 @@ const Header = () => {
                     </Link>
                 </div>
                 <div className="space-x-2">
+                    <ModeToggle />
                     <Button asChild variant={'ghost'}>
                         <Link href={'/cart'} >
                             <ShoppingCartIcon /> Cart
                         </Link>
                     </Button>
-                    <Button asChild variant={'ghost'}>
+                    <Button asChild >
                         <Link href={'/sign-in'} >
                             <UserIcon /> Sign-In
                         </Link>
