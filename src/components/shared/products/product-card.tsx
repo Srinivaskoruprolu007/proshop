c44429459ad5671d00e6f3eb380fbbea48dc7127
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-06 19:04:55 +0530

    added product price and product card

---
+++ src/components/shared/products/product-card.tsx
@@ -0,0 +1,40 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import ProductPrice from "./product-price";
+
+const ProductCard = ({ product }: { product: any }) => {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader className="p-0 items-center">
+        <Link href={`/products/${product.slug}`}>
+          <Image
+            priority={true}
+            src={product.images[0]}
+            width={300}
+            height={300}
+            alt={product.name}
+            className="aspect-square object-cover rounded"
+          />
+        </Link>
+      </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <div className="text-xs font-bold">{product.brand}</div>
+        <Link href={`/products/${product.slug}`}>
+          <h2 className="text-sm font-medium">{product.name}</h2>
+        </Link>
+        <div className="flex-between gap-4">
+          <p>{product.rating} stars</p>
+          {product.stock > 0 ? (
+            <ProductPrice value={Number(product.price)} />
+          ) : (
+            <p className="font-bold text-destructive">Out of stock</p>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
