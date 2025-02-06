8d72330b453736672a4576a4af3a995d81c1d5d8
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-06 19:04:39 +0530

    added product card

--- src/components/shared/products/product-list.tsx
+++ src/components/shared/products/product-list.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
+import ProductCard from "./product-card";
 
-const ProductList = ({ data, title }: { data: any; title?: string }) => {
-    return (
-        <div className='my-10'>
-            <h2 className="h2-bold mb-4">{title}</h2>
-            {data.length > 0 ? (
-                <div className="grid grid-cols-1">
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any[];
+  title?: string;
+  limit?: number;
+}) => {
+  console.log(data);
+  const limitedData = limit ? data.slice(0, limit) : data;
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {limitedData.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product: any) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No Products Found</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-                </div>
-            ) : (
-                <div>
-                    <p>No Products Found</p>
-                </div >
-            )}
-        </div >
-    )
-}
-
-export default ProductList
+export default ProductList;
