073eeda7fd8cdf1b30ad10eca1a14012278387d6
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-03-06 16:18:40 +0530

    added logo and fixed the footer

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,31 +1,34 @@
 "use client";
-import React from 'react'
-import { Button } from '@/components/ui/button';
-import { APP_NAME } from '@/lib/constants';
-import Image from 'next/image';
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
 
 const NotFoundPage = () => {
-    return (
-        <div className='flex flex-col items-center justify-center min-h-screen'
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        priority={true}
+        src={"https://img.icons8.com/arcade/64/shop.png"}
+        width={48}
+        height={48}
+        alt={`${APP_NAME} logo`}
+      />
+      <div className="p-6 rounded-lg shadow-lg w-1/3 text-center">
+        <h1 className="text-xl font-bold mb-4">Not Found</h1>
+        <p className="text-destructive">
+          Coundn&apos;t found requested resources
+        </p>
+        <Button
+          variant={"outline"}
+          className="mt-4 ml-2"
+          onClick={() => (window.location.href = "/")}
         >
-            <Image priority={true}
-                src={'/images/logo.svg'}
-                width={48}
-                height={48}
-                alt={`${APP_NAME} logo`} />
-            <div className='p-6 rounded-lg shadow-lg w-1/3 text-center'>
-                <h1 className='text-xl font-bold mb-4'>Not Found</h1>
-                <p className='text-destructive'>
-                    Coundn&apos;t found requested resources
-                </p>
-                <Button variant={'outline'}
-                    className='mt-4 ml-2'
-                    onClick={() => (window.location.href = '/')}>
-                    Back to home
-                </Button>
-            </div>
-        </div>
-    )
-}
+          Back to home
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { APP_NAME } from '@/lib/constants'
+import React from "react";
+import { APP_NAME } from "@/lib/constants";
 const Footer = () => {
-    const currentYear = new Date().getFullYear()
-    return (
-        <footer className='border-t'>
-            <div className="p-5 flex-center">
-                {currentYear} {APP_NAME}. All Rights reserved.
-            </div>
-        </footer>
-    )
-}
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="border-t">
+      <div className="p-5 flex-center">
+        {currentYear} {APP_NAME}. All Rights reserved.
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
